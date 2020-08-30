f242040c76e765426736c4d35a9dc4f1e7e5017d
Agustin Vignolo <agus88lp@gmail.com>
2020-08-30 17:12:50 -0300

    logica en el carrito de compras

--- js/main.js
+++ js/main.js
@@ -1,72 +1,114 @@
-function Usuario(nombre, correo, password){
-    this.nombre = nombre;
-    this.correo = correo;
-    this.password = password;
+
+//recupero el carrito de productos
+let carrito = document.querySelector('#carrito-productos');
+let cantidad_productos = 0; //la idea es guardar los productos como objeto mas adelante
+let total = 0;
+
+actualizarCarrito();
+
+
+//Funciones
+//agregar un producto al carrito de compras
+function agregarProductoCarrito(imagen, nombre, precio){
+    //crear producto nuevo
+    let producto = crearProductoCarrito(imagen, nombre, precio);
+
+    //agrego el producto al carrito (por ahora solo el precio, mas adelante el objeto producto)
+    cantidad_productos++;
+    total += precio;
+
+    //agrego el producto al listado de productos en el carrito
+    carrito.prepend(producto);
+
+    //actualizo el listado a mostrar en el carrito
+    actualizarCarrito();   
 }
 
-function Producto(nombre, cantidad, precio) {
-    this.nombre = nombre;
-    this.cantidad = cantidad;
-    this.precio = precio;
+function crearProductoCarrito(imagen, nombre, precio){
+    //creo un nuevo contenedor html para el producto
+    let producto = document.createElement('div');
+    producto.classList.add('media');
+
+    //creo el html del nuevo producto en el carrito
+    producto.innerHTML = `
+        <a class="pull-left" href="#">
+            <img  class="media-object" src="images/shop/products/${imagen}" alt="imagen producto">
+        </a>
+        <div class="media-body">
+            <h4 class="media-heading"><a href="#">${nombre}</a></h4>
+            <div class="cart-price">
+            <span>1 x</span>
+            <span>${precio}</span>
+            </div>
+            <h5><strong>$${precio}</strong></h5>
+        </div>
+        <a href="#" class="remove" onclick="eliminarProductoCarrito(event)"><i class="far fa-times-circle"></i></a>
+    `;
+    return producto;
 }
 
-function Carrito(){
-    let carrito = [];
-
-    this.obtenerProductos = () => carrito;
-
-    this.agregarProducto = (producto) => carrito.push(producto);
+//eliminar un producto del carrito de compras
+function eliminarProductoCarrito(event){
+    //elimino el producto del DOM
+    let producto = event.target.parentNode.parentNode;
+    producto.parentNode.removeChild(producto);
     
-    this.eliminarProducto = () => {
-        //hay que implementar la eliminación
-    };
+    //elimino el producto del arreglo de productos en el carrito
+    let precio = event.target.parentNode.parentNode.childNodes[3].textContent.split('$');
+    total -= precio[1]; 
+    cantidad_productos--;
 
-    this.comprar = () => {
-        //hay que implementar la compra
-    }
-
-    this.validarStock = () => {
-        //hay que implementar la validación del stock
-    }
-
+    //actualizo el listado a mostrar en el carrito
+    actualizarCarrito();
 }
 
-//implementar todo el modulo de usuarios, en caso de darlo en el curso.
-function Usuarios() {
-    let usuarios = [];
+//visualizar un producto en el modal, al hacer click en la lupa sobre cada producto.
+function verProducto(imagen, nombre, precio){
+    document.querySelector('.modal-image img').setAttribute("src", `images/shop/products/${imagen}`);
+    document.querySelector('.product-title').textContent = nombre;
+    document.querySelector('.product-price').textContent = `$${precio}`;
+}
 
-    this.obtenerUsuarios = () => usuarios;
+function actualizarCarrito(){
+    if(total === 0){
+        let texto = document.createElement('h4');
+        texto.setAttribute("id", "sin-productos");
+        texto.textContent = 'No hay productos agregados';
+        carrito.appendChild(texto);
 
-    this.agregarUsuario = (usuario) => usuarios.push(usuario);
-    
-    this.eliminarUsuario = () => {
-        //hay que implementar la eliminación
-    };
+        let total = document.querySelector('#carrito-total');
+        if(total){
+            total.parentNode.removeChild(total);
+        }
+    }else if(cantidad_productos === 1 && !document.querySelector('#carrito-total')){
+        //elimino mensaje que no hay productos agregados
+        let texto = document.querySelector('#sin-productos');
+        if(texto){
+            texto.parentNode.removeChild(texto);
+        }
 
-    this.login = (usuario) => {
-        //hay que implementar el login
-    }
-
-    this.logout = (usuario) => {
-        //hay que implementar el logout
+        //agrego el total y los botones para realizar la compra y ver carrito
+        let botones = carritoBotones(total);
+        carrito.appendChild(botones);
+    }else{
+        //actulizo el precio total
+        document.querySelector('.total-price').textContent = total;
     }
 }
 
-//obtengo una instancia del carrito
-let carrito = new Carrito();
 
-//obtengo instancias de algunos productos
-let produco1 = new Producto("Remera", 1, 150);
-let produco2 = new Producto("Bufanda", 2, 50);
-let produco3 = new Producto("Jean", 1, 300);
-
-//agrego productos al carrito
-carrito.agregarProducto(produco1);
-carrito.agregarProducto(produco2);
-carrito.agregarProducto(produco3);
-
-//obtengo el listado de productos del carrito
-let listado = carrito.obtenerProductos();
-
-//imprimo el listado de productos del carrito
-console.log(listado);  
+function carritoBotones(total){
+    let info = document.createElement('div');
+    info.setAttribute("id", 'carrito-total');
+    info.innerHTML = `
+        <div class="cart-summary">
+            <span>Total: </span>
+            <span class="total-price">$${total}</span>
+        </div>
+        <ul class="text-center cart-buttons">
+            <li><a href="#" class="btn btn-small">Ver carrito</a></li>
+            <li><a href="#" class="btn btn-small btn-solid-border">Comprar</a></li>
+        </ul>
+    `;
+    return info;
+}
